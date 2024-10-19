92d7f1700698c30db36135675b0ece400e11b044
임현수(karegoie) <87375159+karegoie@users.noreply.github.com>
2024-10-19 16:55:41 +0900

    conflict resolve

--- src/cwt/mod.rs
+++ src/cwt/mod.rs
@@ -91,19 +91,50 @@ fn cwt_perform(f: &Array1<Complex<f64>>, opt: &Params) -> Array2<f64> {
     result_cwt_perform.to_owned()
 }
 
-pub fn _normalize(matrix: &mut Array2<f64>) {
-  for mut row in matrix.axis_iter_mut(Axis(0)) {
-        let min = row.fold(f64::MAX, |a, &b| a.min(b));
-        let max = row.fold(f64::MIN, |a, &b| a.max(b));
-        let range = max - min;
-        
-        if range != 0.0 {
-            row.mapv_inplace(|x| (x - min) / range);
-        } else {
-            row.fill(0.5);
-        }
+// pub fn normalize(matrix: &mut Array2<f64>) {
+//     let mut min = f64::MAX;
+//     let mut max = f64::MIN;
+//
+//     for row in matrix.axis_iter(Axis(0)) {
+//         for value in row.iter() {
+//             if *value < min {
+//                 min = *value;
+//             }
+//             if *value > max {
+//                 max = *value;
+//             }
+//         }
+//     }
+//
+//     let range = max - min;
+//
+//     //matrix.mapv_inplace(|x| (((x - min) / range) +1.0).log10());
+//     matrix.mapv_inplace(|x| (x - min) / range);
+// }
+
+pub fn normalize(matrix: &mut Array2<f64>) -> Array2<f64> {
+    //let mut min = f64::MAX;
+    //let mut max = f64::MIN;
+    let mut z = Vec::new();
+    let sh = matrix.shape();
+    for row in matrix.axis_iter(Axis(0)) {
+        //for value in row.iter() {
+        //if *value < min {
+        //  min = *value;
+        //}
+        //if *value > max {
+        //   max = *value;
+        // }
+        // }
+        z.extend(&z_norm(&row.to_vec()));
     }
+    Array2::from_shape_vec((sh[0],sh[1]),z).unwrap()
+    //let range = max - min;
+
+    //matrix.mapv_inplace(|x| (((x - min) / range) +1.0).log10());
+    //matrix.mapv_inplace(|x| (x - min) / range);
 }
+
 #[derive(Clone)]
 pub struct CwtIterator {
     sig_seqs: Array2<Complex<f64>>,
@@ -161,16 +192,16 @@ impl Iterator for CwtIterator {
     type Item = Array2<f64>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        
-        if self.current_batch >= (self.sig_seqs.dim().0 + self.batch_size - 1) / self.batch_size {
-            return None;
+
+        if self.current_batch > (self.sig_seqs.dim().0 / self.batch_size) {
+            return None
         }
-        info!("Current batch: {}", self.current_batch + 1);
-        
+
+
         let start = self.current_batch * self.batch_size;
+        //println!("shape: {:?}", self.sig_seqs.dim());
         let end = std::cmp::min(start + self.batch_size, self.sig_seqs.dim().0);
         let batch = self.sig_seqs.slice(s![start..end, ..]).to_owned();
-
         let mut batch_cwt = Array2::<f64>::zeros((self.opt.num, batch.dim().0));
         batch.axis_iter(Axis(1)).for_each(|f| {
             let one_base_result = cwt_perform(&f.to_owned(), &self.opt);
@@ -178,8 +209,9 @@ impl Iterator for CwtIterator {
         });
 
         self.current_batch += 1;
-        _normalize(&mut batch_cwt);
-        //_standardize(&mut batch_cwt);
+        if self.opt.only {
+            batch_cwt = normalize(&mut batch_cwt);
+        }
         Some(batch_cwt)
     }
 }
