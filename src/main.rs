35d346fae15be7c2a5f6745e5a0faf1b55fd6ac2
Hyunsu Lim <karegoie@snu.ac.kr>
2024-10-29 21:52:05 +0900

    change default

--- src/main.rs
+++ src/main.rs
@@ -147,7 +147,7 @@ fn process_sequence_fasta(
         None => {
             let mut sorted_diversity = shannon_diversity.clone();
             sorted_diversity.sort_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal));
-            let index = (sorted_diversity.len() as f64 * 0.07).ceil() as usize;
+            let index = (sorted_diversity.len() as f64 * 0.01).ceil() as usize;
             let threshold = sorted_diversity[index.min(sorted_diversity.len() - 1)];
             info!("Calculated threshold: {}", threshold);
             threshold
@@ -498,7 +498,7 @@ fn main() -> Result<(), std::io::Error> {
                 None => {
                     let mut sorted_diversity = results_locked.iter().map(|d| d.diversity).collect::<Vec<_>>();
                     sorted_diversity.sort_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal));
-                    let index = (sorted_diversity.len() as f64 * 0.07).ceil() as usize;
+                    let index = (sorted_diversity.len() as f64 * 0.01).ceil() as usize;
                     let threshold = sorted_diversity[index.min(sorted_diversity.len() - 1)];
                     info!("Calculated threshold: {}", threshold);
                     threshold
